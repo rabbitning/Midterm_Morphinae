c2c2b85982cc579e9c8c91612d235fd3a25c619d
rabbitning <s111019023@stu.ntue.edu.tw>
2022-11-02 14:44:45 +0800

    nav2.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 $(() => {
     $('body').prepend(`
     <div class="op">
@@ -7,14 +5,7 @@ $(() => {
         <div class="logo_text">MORPHINAE</div>
     </div>
     `);
-
     setTimeout(() => {
         $('.op').fadeOut(500);
     }, 1500);
-    //location.reload();
-
-    // $('.nav_icon').click(() => {
-    //     // $('.nav_icon').toggleClass('.icon_move_left');
-    //     $('.nav_list').slideToggle(300);
-    // })
-})
+});
