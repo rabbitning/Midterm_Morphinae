457057f8e38147eb5f19d578840a1690f0f0e969
rabbitning <s111019023@stu.ntue.edu.tw>
2022-11-02 10:36:08 +0800

    menu0.1

--- js/index.js
+++ js/index.js
@@ -1,33 +1,68 @@
-
 $(() => {
     // setTimeout(() => {
     //     $('.op').fadeOut(800);
     // }, 4500);
     // $('body').fadeIn(1000);
-    
 
 
-        
-    
+
+
+
 
 
     $(document).mousemove((e) => {
         $('.follow_ms').css({ 'left': e.pageX - 50, 'top': e.pageY - 50 });
-        $('.bgimg').css({ 'left': e.pageX / -50, 'top': e.pageY / -50 });
+        $('.bgimg').css({ 'left': ((-0.05 * $(window).width()) + ((e.pageX - ($(window).width() / 2)) / -20)), 'top': ((-0.5 * $(window).height()) + ((e.pageY - ($(window).height() / 2)) / -20)) });
+        // console.log(((-0.1 * $(window).width()) + ((e.pageX - ($(window).width() / 2)) / -10)));
     });
 
-    $('.content').hover(() => {
-        $('.follow_ms').css('transform', 'scale(2)');
+    $('.close_btn').click(() => {
+        $('body').prepend(`
+        <div class="mask">
+        </div>
+        `);
+        setTimeout(() => {
+            $('.mask').remove();
+        }, 50);
+    });
+    $('.menu').hover(() => {
+        setTimeout(() => {
+            $('.menu').append(`
+            <div class="menu_header menu_item">
+                <img src="./img/morphinae_logo_white.webp" alt="" class="menu_header_item icon">
+                <div class="menu_header_item title">MORPHINAE</div>
+                <div class="menu_header_item close_btn">╳</div>
+            </div>
+            <div class="menu_content menu_item">
+                <a href="./story.html" class="menu_content_story menu_content_item">
+                    <div class="story_title">Story</div>
+                </a>
+                <a href="./system.html" class="menu_content_system menu_content_item">
+                    <div class="system_title">System</div>
+                </a>
+            </div>
+            <div class="menu_contact menu_item">
+                <a href="./contact.html" class="contact_text">Contact Us</a>
+            </div>
+            `)
+        }, 800);
     },
         () => {
-            $('.follow_ms').css('transform', 'scale(1)');
-        });
-    //let r = 0;
-    // setInterval(() => {
-    //     $('.circle').css('transform', 'rotate3D(' + 2 + ',' + 2 + ', ' + 1 + ', ' + r + 'deg)');
-    //     r++;
-    //     if (r >= 360) {
-    //         r = 0;
-    //     }
-    // }, 10);
+            $('.menu').empty();
+        }
+    );
+
+
+
+    // $('.content').hover(()=>{
+
+    //     setTimeout(()=>{
+    //         $('.menu').css('display', 'block');
+    //     }, 2000);
+    // },()=>{
+    //     $('.menu').css('display', 'none');
+    // }
+    // );
+
+
 });
--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ $(()=>{
     $('body').prepend(`
     <div class="op">
         <img src="./img/morphinae_logo.webp" class="logo">
-        <div class="logo_text">MORPINAE</div>
+        <div class="logo_text">MORPHINAE</div>
     </div>
     `);
 
     setTimeout(()=>{
-        $('.op').fadeOut(1500);
-    }, 3000);
+        $('.op').fadeOut(500);
+    }, 1500);
     //location.reload();
 })
