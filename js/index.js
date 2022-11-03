bb8bc42d4e13204046582379036c9a149d6c4cbc
rabbitning <s111019023@stu.ntue.edu.tw>
2022-11-04 03:48:08 +0800

    final

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let tid;
 $(() => {
     $(document).mousemove((e) => {
         $('.bgimg').css({ 'left': ((-0.05 * $(window).width()) + ((e.pageX - ($(window).width() / 2)) / -20)), 'top': ((-0.5 * $(window).height()) + ((e.pageY - ($(window).height() / 2)) / -20)) });
@@ -8,15 +9,18 @@ $(() => {
             $('.menu').css('display', 'block');
         }, 50);
     });
-    $('.content').hover(() => {
-        $('.home_title').fadeOut();
-        setTimeout(() => {
-            $('.menu_content_item').css('display', 'flex');
-            $('.menu_contact').css('display', 'flex');
+    $('.content').mouseenter(() => {
+        tid = setTimeout(() => {
+            $('.menu_box').fadeIn(200);
         }, 2000);
-    }, () => {
-        $('.home_title').fadeIn();
-        $('.menu_content_item').css('display', 'none');
-        $('.menu_contact').css('display', 'none');
+        $('.home_title').fadeOut(300);
+    });
+    $('.content').mouseleave(() => {
+        clearTimeout(tid);
+        $('.menu_box').fadeOut(200);
+        $('.home_title').fadeIn(300);
+    });
+    $('.menu_box').mouseenter(() => {
+        $('.menu_box').fadeIn(200);
     });
 });
--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
 $(() => {
-    $('body').prepend(`
-    <div class="op">
-        <img src="./img/morphinae_logo.webp" class="logo">
-        <div class="logo_text">MORPHINAE</div>
-    </div>
-    `);
-    $('img').ready(() => {
+    $(window).ready(() => {
         setTimeout(() => {
-            $('.op').fadeOut(500);
+            $('.loading').fadeOut(1000);
         }, 1500);
     });
 });
